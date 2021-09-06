1ff5f06333b1c9fd27ed7d159a0d8b3077debc63
Alaa <78312680+alaaelsanteel@users.noreply.github.com>
2021-09-06 18:29:14 +0200

    Add files via upload

---
+++ programming fundamentals/Function_1.py
@@ -0,0 +1,6 @@
+def favourit_city(name):
+    
+    print ("One of my favourit cities is" , name)
+favourit_city("cairo")
+favourit_city("alex")
+favourit_city("aswan")
---
+++ programming fundamentals/condition.py
@@ -0,0 +1,9 @@
+
+food = input ("What is your favorite food ?")
+if food == "pizza":
+     print("Yep! so amazing")
+
+else:
+     print("Yuck! That is not it ")
+
+print("Thanks for playing")
