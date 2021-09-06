61a93f87fd21c74b4445576294065cb74e3d7b08
Alaa <78312680+alaaelsanteel@users.noreply.github.com>
2021-09-06 18:28:21 +0200

    Delete Function_2.py

--- Function_2.py
+++
@@ -1,9 +0,0 @@
-def withdraw_money(current_balance , amount ):
-    if(current_balance >= amount):
-       current_balance -=amount
-    return current_balance
-balance = withdraw_money(100,80)
-if (balance<=50):
-    print("we need to make a deposit")
-else :
-    print("nothing to see here!")
