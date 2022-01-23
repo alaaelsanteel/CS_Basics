0a4d13e2e178432b3701e804dbf9671a5f139e61
Alaa Elsanteel <78312680+alaaelsanteel@users.noreply.github.com>
2022-01-23 03:56:44 +0200

    Add files via upload

---
+++ Object Oriented Design&programming/inherance2/inherance2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+class base
+{
+	int x;
+public :
+	base()   //defult costractor
+	{
+		x = 0;
+		cout << "base defult constractor\n";
+	}
+
+};
+ 
+class derived : public base
+{
+	int y;
+public :
+	derived()    //defult constractor 
+	{
+		y = 0;
+		cout << "derived defult constractor \n";
+	}
+	derived(int i)      //parameterized constractor
+	{
+		y = i;
+		cout << "derived parameterized\n";
+	}
+
+
+};
+
+
+void main()
+{
+	base b;
+	//the output will be the(base) defult constractor only
+
+	derived d1;         
+	//no parameters means we're using the defult constractor
+	//the output will be both the(base) defult constractor and (derived)defult constractor
+
+	derived d2(10);      
+	//paramrterized constractor
+	//the output will be both the(base) defult constractor and (derived)defult constractor
+
+
+
+}
+
+
